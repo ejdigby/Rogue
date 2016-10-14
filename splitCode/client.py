c08b7e715a9c9167190747061816a84ef3038b60
Edward Digby <edward@ejdigby.com>
2016-10-14 15:04:47 +0100

    Added client send/recieve function and class

--- splitCode/client.py
+++ splitCode/client.py
@@ -1,4 +1,47 @@
 import os
+from socket import *
+import threading
+from Robot import Robot
+import pickle
+from io import BytesIO
+
+SIZE =1024
+
+class client(threading.Thread):
+    def __init__(self,soc):
+        threading.Thread.__init__(self)
+        self.soc = soc
+        self.RunningState = True
+
+    def iRecieve(self):
+        data = self.soc.recv(SIZE)
+        return data
+
+    def run(self):
+        while self.RunningState:
+            msg = self.iRecieve()
+            print('Recieved-> ',msg.decode())
+
+
+
+
+soc1 = socket(AF_INET,SOCK_STREAM)
+soc1.connect(('127.0.0.1',5432))
+soc1.send('SEND'.encode()) # telling server we will send data from here
+
+soc2 = socket(AF_INET,SOCK_STREAM)
+soc2.connect(('127.0.0.1',5432))
+soc2.send('RECV'.encode()) # telling server we will recieve data from here
+
+def iSend(conn,msg):
+    conn.sendall(msg)
+    print("Sent->  ", msg)
+
+
+thrd = client(soc2)
+thrd.start()
+
+
 #can we please figure out what this actually does? or at least someone tell me since idk
 os.environ['LINES'] = '40'
 os.environ['COLUMNS']= '80'
